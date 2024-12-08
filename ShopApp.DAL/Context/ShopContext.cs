c4471fbe70fc3d6230d1bc9dc5a59d82e947b47c
Broly 12 <brailynperezbatista@gmail.com>
2024-12-08 01:49:08 -0400

    Creado el Contexto

---
+++ ShopApp.DAL/Context/ShopContext.cs
@@ -0,0 +1,24 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using ShopApp.DAL.Entities;
+
+namespace ShopApp.DAL.Context
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(DbContextOptions<ShopContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Categories> Categories { get; set; }
+        public DbSet<Customers> Customers   { get; set; }
+        public DbSet<Employees> Employees   { get; set; }
+        public DbSet<OrderDetails> OrderDetails { get; set; }
+        public DbSet<Orders> orders { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<Shippers> Shippers { get; set; }
+        public DbSet<Suppliers> Suppliers { get; set; }
+
+    }
+}
